1cdbb88623c368a9c1a6cae23489f48ad760793a
Geert van Horrik <geert@catenalogic.com>
2026-01-05 17:18:14 +0100

    Add service collection helper

---
+++ src/Orc.Serialization.Json.Tests/Helpers/ServiceCollectionHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace Orc.Serialization.Tests
+{
+    using Catel;
+    using Microsoft.Extensions.DependencyInjection;
+    using Orc.Serialization.Json;
+
+    internal static class ServiceCollectionHelper
+    {
+        public static IServiceCollection CreateServiceCollection()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddLogging();
+            serviceCollection.AddCatelCore();
+            serviceCollection.AddOrcSerializationJson();
+
+            return serviceCollection;
+        }
+    }
+}
---
+++ src/Orc.Serialization.Yaml.Tests/Helpers/ServiceCollectionHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace Orc.Serialization.Tests
+{
+    using Catel;
+    using Microsoft.Extensions.DependencyInjection;
+    using Orc.Serialization.Yaml;
+
+    internal static class ServiceCollectionHelper
+    {
+        public static IServiceCollection CreateServiceCollection()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddLogging();
+            serviceCollection.AddCatelCore();
+            serviceCollection.AddOrcSerializationYaml();
+
+            return serviceCollection;
+        }
+    }
+}
