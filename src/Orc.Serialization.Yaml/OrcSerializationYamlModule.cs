0bd70549d2ee7406465fa7865614380f53c6aed1
Geert van Horrik <geert@catenalogic.com>
2026-01-06 17:52:53 +0100

    Update

--- src/Orc.Serialization.Yaml/OrcSerializationYamlModule.cs
+++ src/Orc.Serialization.Yaml/OrcSerializationYamlModule.cs
@@ -1,10 +1,10 @@
-﻿namespace Orc.Serialization.Yaml
+﻿namespace Orc
 {
-    using System.Xml.Serialization;
     using Catel.Services;
     using Catel.ThirdPartyNotices;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.DependencyInjection.Extensions;
+    using Orc.Serialization.Yaml;
 
     /// <summary>
     /// Core module which allows the registration of default services in the service collection.
